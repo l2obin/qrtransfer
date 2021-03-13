6d951d0c6a7d1d277791b607076f219e79d16bc0
Robin Ang <robinang87@gmail.com>
2021-03-13 19:48:37 +0800

    feat: implement simple data processing on receiver

--- src/receiver/Receiver.tsx
+++ src/receiver/Receiver.tsx
@@ -10,6 +10,8 @@ type ReceiverState = {
   videoConstraints?: MediaStreamConstraints["video"],
   currentFacingMode: CameraFacingModes,
   decodedMessage: string,
+
+  packets?: Array<string>,
 }
 
 enum CameraFacingModes {
@@ -24,10 +26,49 @@ class Receiver extends React.Component<ReceiverProps, ReceiverState> {
   constructor(props: ReceiverProps) {
     super(props);
     this.onMessageChanged = this.onMessageChanged.bind(this);
+    this.processData = this.processData.bind(this);
+    this.restart = this.restart.bind(this);
   }
 
   onMessageChanged(newMessage: string) {
     this.setState({ decodedMessage: newMessage });
+    this.processData(newMessage);
+  }
+
+  processData(newMessage: string) {
+    //const regex = /(\d+)\/(\d+)\|(.*)/;
+    const regex = /(?<current_packet>\d+)\/(?<total_packet>\d+)\|(?<data>.*)/;
+    let matches = regex.exec(newMessage);
+    if (this.state && matches && 
+      matches.length >= 3 && 
+      matches.groups &&
+      matches.groups['current_packet'] && 
+      matches.groups['total_packet'] &&
+      matches.groups['data']) {
+      
+      let currentPacket = Number(matches.groups['current_packet'])
+      let totalPacket = Number(matches.groups['total_packet'])
+      let data = matches.groups['data'];
+
+      this.setState( state => {
+        let packets = state.packets;
+        if (!packets) {
+          packets = new Array<string>(totalPacket);
+        }
+        packets[currentPacket] = data;
+        return {packets};
+      });
+    }
+    else {
+      if (matches) alert('data too short' + matches.length);
+      else alert('data too short');
+    }
+  }
+
+  restart() {
+    this.setState({
+      packets: undefined
+    });
   }
 
   render() {    
@@ -37,6 +78,13 @@ class Receiver extends React.Component<ReceiverProps, ReceiverState> {
       <Box>
         <QRScanner ref={this.qrScannerRef} messageChange={this.onMessageChanged} />
       </Box>
+      <Button variant="contained" color="secondary" onClick={this.restart} >Restart</Button>
+      {this.state && this.state.packets ? 
+      <Box style={{backgroundColor: '#333'}}> 
+        {this.state.packets.map((value, index) => {
+          return value ? (<span>{value}</span>) : (<span>---</span>)
+        })}
+      </Box>: null}
       {/* <Button variant="contained" color="primary" onClick={this.toggleCamera} >Toggle Camera</Button> */}
       {/* <Button variant="contained" color="primary" onClick={this.receive} >Receive</Button> */}
     </Container>;
