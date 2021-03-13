19d32e35d8ce524b653ecd8ddb10e6f64ed612be
Robin Ang <robinang87@gmail.com>
2021-03-13 18:38:12 +0800

    feat: added progress bar in send function

--- src/sender/Sender.tsx
+++ src/sender/Sender.tsx
@@ -1,45 +1,59 @@
 import React from 'react';
 import './Sender.css';
 
-import { Button, Container, Box } from '@material-ui/core';
+import { Button, Container, Box, Slider, Typography } from '@material-ui/core';
 import QRCode from 'qrcode.react';
+import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from 'node:constants';
 
 const EACH_PACKET_SIZE = 30;
 
 type SenderState = {
-  each_packet_size: number,
-  data: string,
+  eachPacketSize: number,
+  data?: string,
 
-  current_packet_number: number,
-  total_number_of_packets: number,
-  current_packet_data: string,
+  currentPacketNumber: number,
+  totalNumberOfPackets: number,
+  currentPacketData?: string,
 }
 
-class Sender extends React.Component<{}, SenderState> {
+type SenderProps = {}
 
-  tick() {
-    let nextPacket = (this.state.current_packet_number + 1) % this.state.total_number_of_packets;
+class Sender extends React.Component<SenderProps, SenderState> {
+
+  constructor(props: SenderProps) {
+    super(props);
+
+    this.state = {
+      eachPacketSize: EACH_PACKET_SIZE,
+      currentPacketNumber: 0,
+      totalNumberOfPackets: 0,
+    }
+
+    setInterval(() => this.tick(), 100);
+
+    this.send = this.send.bind(this);
+  }
+
+  send() {
+    console.log(this);
+    const data = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    const totalNumberOfPackets = Math.ceil(data.length / this.state.eachPacketSize);
+
     this.setState({
-      current_packet_number: nextPacket,
-      current_packet_data: this.getCurrentPacket(this.state.data, this.state.each_packet_size, nextPacket, this.state.total_number_of_packets),
+      data,
+      currentPacketNumber: 0,
+      totalNumberOfPackets: totalNumberOfPackets,
+      currentPacketData: this.getCurrentPacket(data, this.state.eachPacketSize, 0, totalNumberOfPackets),
     });
   }
 
-  prepare() {
-    if (this.state.data === null || this.state.data.length === 0) {
-      this.setState({
-        current_packet_number: 0,
-        total_number_of_packets: 0,
-        current_packet_data: ''
-      })
-      return;
-    }
+  tick() {
+    if (!this.state.data) return; // no data to send
 
-    const totalNumberOfPackets = Math.ceil(this.state.data.length / this.state.each_packet_size);
+    let nextPacket = (this.state.currentPacketNumber + 1) % this.state.totalNumberOfPackets;
     this.setState({
-      current_packet_number: 0,
-      total_number_of_packets: totalNumberOfPackets,
-      current_packet_data: this.getCurrentPacket(this.state.data, this.state.each_packet_size, 0, totalNumberOfPackets),
+      currentPacketNumber: nextPacket,
+      currentPacketData: this.getCurrentPacket(this.state.data, this.state.eachPacketSize, nextPacket, this.state.totalNumberOfPackets),
     });
   }
 
@@ -47,27 +61,31 @@ class Sender extends React.Component<{}, SenderState> {
     return (currentPacketNumber+1) + '/' + totalNumberOfPackets + "|" + data.substr(currentPacketNumber * packetSize, packetSize);
   }
 
-  componentWillMount() {
-    this.setState({
-      data: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      each_packet_size: EACH_PACKET_SIZE,
-    });
-  }
-
-  componentDidMount() {
-    this.prepare();
-    setInterval(() => this.tick(), 100);
-  }
-
   render() {
     return <Container maxWidth="sm">
       <h3>Send</h3>
       
-      <Button variant="contained" color="primary">Send</Button>
-      <span>{this.state.current_packet_number+1}</span>/<span>{this.state.total_number_of_packets}</span>
-      <Box alignItems="center" style={{backgroundColor: 'white', padding: 10, width: "74vmin", height: "74vmin"}}>
-        { this.state.current_packet_data != null ? <QRCode value={this.state.current_packet_data} renderAs='svg' style={{width: "70vmin", height: "70vmin"}} /> : null }
-      </Box>
+      { this.state ? <Box>
+        <Box alignItems="center" style={{backgroundColor: '#444', padding: 10, width: "60vmin", height: "60vmin"}}>
+          { this.state.currentPacketData != null ? <QRCode value={this.state.currentPacketData} renderAs='svg' style={{width: "55vmin", height: "55vmin"}} /> : null }
+        </Box>
+        <Typography id="send-progress" gutterBottom>
+          Progress <span>{this.state.currentPacketNumber+1}</span>/<span>{this.state.totalNumberOfPackets}</span>
+        </Typography>
+        <Slider
+          value={this.state.currentPacketNumber+1}
+          //getAriaValueText={(this.state.currentPacketNumber+1).toString()}
+          aria-labelledby="send-progress"
+          step={1}
+          marks
+          min={1}
+          max={this.state.totalNumberOfPackets}
+          valueLabelDisplay="auto"
+          style={{width: "74vmin"}}
+        />
+      </Box>: null}
+      <Button variant="contained" color="primary" onClick={this.send}>Send</Button>
+      
     </Container>;
   }
 }
